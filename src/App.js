236c57e6835ae9144a02a6e102de1d3838fff9f4
minoka kakizaki <minoka.kakizaki@gmail.com>
2021-12-14 15:42:09 -0800

    got rhe nav link ans stuff to work properly

--- src/App.js
+++ src/App.js
@@ -1,7 +1,37 @@
 import './App.css';
+import { BrowserRouter, NavLink, Switch, Route } from 'react-router-dom';
+import Main from './views/Main/Main';
+import Films from './views/Films/Films';
+import Characters from './views/Characters/Characters';
+import Books from './views/Books/Books';
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <header>
+          <NavLink to="/">Main</NavLink>
+          <NavLink to="/films">Films</NavLink>
+          <NavLink to="/characters">Characters</NavLink>
+          <NavLink to="/books">Books</NavLink>
+        </header>
+        <Switch>
+          <Route exact path="/">
+            <Main></Main>
+          </Route>
+          <Route path="/films">
+            <Films></Films>
+          </Route>
+          <Route path="/characters">
+            <Characters></Characters>
+          </Route>
+          <Route path="/books">
+            <Books></Books>
+          </Route>
+        </Switch>
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/views/Characters/Characters.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function Characters() {
+  return (
+    <div>
+      <h1>Characters page</h1>
+    </div>
+  );
+}
