3449008891958271c0af0ea41bc2de99a2e55b6a
minoka kakizaki <minoka.kakizaki@gmail.com>
2021-12-14 16:16:31 -0800

    got the films to fetch

--- src/views/Films/Films.js
+++ src/views/Films/Films.js
@@ -1,6 +1,17 @@
-import React from 'react';
+import { useState, React, useEffect } from 'react';
+import { fetchFilms } from '../../services/films';
 
 export default function Films() {
+  const [films, setFilms] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const data = await fetchFilms();
+      setFilms(data);
+    };
+    fetchData();
+  }, []);
+  console.log(films);
   return (
     <div>
       <h1>Films page</h1>
